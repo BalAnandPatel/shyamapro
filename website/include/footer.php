1f8a8472147e79412e9bbd46c5c01586762f7c1b
YashMaury <yasmaurya2000@gmail.com>
2023-08-16 17:58:11 +0530

    upd

--- website/include/footer.php
+++ website/include/footer.php
@@ -1,211 +1,102 @@
+<br><br>
 
-        <!--Site Footer Start-->
-        <footer class="site-footer">
-            <div class="site-footer__top">
-                <div class="container">
-                    <div class="site-footer__top-inner">
-                        <div class="site-footer-shape-1 float-bob-x"
-                            style="background-image: url(assets/images/shapes/site-footer-shape-1.png);"></div>
-                        <div class="row">
-                            <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="100ms">
-                                <div class="footer-widget__column footer-widget__about">
-                                    <div class="footer-widget__logo">
-                                        <a href="index.html"><img src="assets/images/logo/2-1.png"
-                                                alt=""></a>
-                                    </div>
-                                    <div class="footer-widget__about-text-box">
-                                        <p class="footer-widget__about-text">SHYAMAVSVSS KRISHI LIMITED</p>
-                                    </div>
-                                </div>
+<section class="content footer_logo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="">
+                    <div class="swiper mySwiper0">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important">
+                                <a class="redirectconfirmation" href="http://cashlessindia.gov.in/" target="_blank"><img alt="cashlessindia" src="assets/images/minister/cashlessindia.png"></a>
                             </div>
-                            <div class="col-xl-2 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="200ms">
-                                <div class="footer-widget__column footer-widget__Explore">
-                                    <div class="footer-widget__title-box">
-                                        <h3 class="footer-widget__title">Explore</h3>
-                                    </div>
-                                    <ul class="footer-widget__Explore-list list-unstyled">
-                                        <li><a href="index.php">Home</a></li>
-                                        <li><a href="about.php">About</a></li>
-                                        <li><a href="service_agriculture.php">Services</a></li>
-                                        <li><a href="blog_details_agriculture.php">Blogs</a></li>
-                                        <li><a href="contact.php">Contact Us</a></li>
-                                        <li><a href="cancelreturn.php">Cancel Return</a></li>
-                                        <li><a href="shipping.php">Shipping</a></li>
-                                        <li><a href="privacy_policy.php">Privacy Policy</a></li>
-                                        <li><a href="terms.php">Terms & Conditions</a></li>
-                                    </ul>
-                                </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://pgportal.gov.in/" target="_blank"><img alt="public" src="assets/images/minister/public.png"></a>
                             </div>
-                            <div class="col-xl-3 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="300ms">
-                                <div class="footer-widget__column footer-widget__news">
-                                    <div class="footer-widget__title-box">
-                                        <h3 class="footer-widget__title">News</h3>
-                                    </div>
-                                    <ul class="footer-widget__news-list list-unstyled">
-                                        <li>
-                                            <div class="footer-widget__news-img">
-                                                <img src="assets/images/resources/footer-widget-news-img-1.jpg" alt="">
-                                            </div>
-                                            <div class="footer-widget__news-content">
-                                                <p class="footer-widget__news-date">20 Jul, 2022</p>
-                                                <h5 class="footer-widget__news-sub-title"><a href="blog-details.html">A
-                                                        Organic Food
-                                                        Gives
-                                                        More Good Taste</a></h5>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="footer-widget__news-img">
-                                                <img src="assets/images/resources/footer-widget-news-img-2.jpg" alt="">
-                                            </div>
-                                            <div class="footer-widget__news-content">
-                                                <p class="footer-widget__news-date">20 Jul, 2022</p>
-                                                <h5 class="footer-widget__news-sub-title"><a href="blog-details.html">A
-                                                        Organic Food
-                                                        Gives
-                                                        More Good Taste</a></h5>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
+
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://igod.gov.in/" target="_blank"><img alt="webdirectory" src="assets/images/minister/webdirectory.png"></a>
                             </div>
-                            <div class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="400ms">
-                                <div class="footer-widget__column footer-widget__Contact">
-                                    <div class="footer-widget__title-box">
-                                        <h3 class="footer-widget__title">Contact</h3>
-                                    </div>
-                                    <ul class="footer-widget__Contact-list list-unstyled">
-                                        <li>
-                                             <div class="text">
-                                                <p><a href="#">Helpline No.</a></p>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            
-                                            <div class="icon">
-                                                <span class="fas fa-phone-square-alt"></span>
-                                            </div>
-                                            
-                                            <div class="text">
-                                                <p><a href="#">91+ 9919412317</a></p>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="icon">
-                                                <span class="fas fa-envelope"></span>
-                                            </div>
-                                            <div class="text">
-                                                <p><a href="#">vikasnishad7863@gmail.com</a></p>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="icon">
-                                                <span class="icon-pin"></span>
-                                            </div>
-                                            <div class="text">
-                                                <p>C/O DASRATH PAUL, OM NAGAR COLONY ARATEE NO. 86/320, SARNATH VARANASI Varanasi UP 221007 IN</p>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                 <!--    <form class="footer-widget__Contact-form">
-                                        <div class="footer-widget__Contact-input-box">
-                                            <input type="email" placeholder="Email Address" name="email">
-                                            <button type="submit" class="footer-widget__Contact-btn"><i
-                                                    class="icon-right-arrow"></i></button>
-                                        </div>
-                                    </form> -->
-                                </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://www.digitalindia.gov.in/" target="_blank"><img alt="degitalindia" src="assets/images/minister/degitalindia.png"></a>
                             </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a  class="redirectconfirmation" href="https://www.mygov.in/" target="_blank"><img alt="mygov" src="assets/images/minister/mygov.png"></a>
+                            </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://www.makeinindia.com/home" target="_blank"><img alt="makeinindia" src="assets/images/minister/makeinindia.png"></a>
+                            </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://data.gov.in/" target="_blank"><img alt="datagov" src="assets/images/minister/datagov.png"></a>
+                            </div>
+                            <div class="swiper-slide swiper-slide_" style="width:150px !important;height: 150px !important;">
+                                <a class="redirectconfirmation"  href="https://www.india.gov.in/" target="_blank"><img alt="indiagov" src="assets/images/minister/indiagov.png"></a>
+                            </div>
+
                         </div>
+                        <div class="swiper-pagination"></div>
                     </div>
                 </div>
             </div>
-            <div class="site-footer__bottom">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-12">
-                            <div class="site-footer__bottom-inner">
-                                <p class="site-footer__bottom-text">© Copyright 2022 by <a href="http://krishilimited.com/">Krishi Limited India</a></p>
-                                <div class="site-footer__social">
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-facebook"></i></a>
-                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    <a href="#"><i class="fab fa-instagram"></i></a>
-                                </div>
-                                <div class="site-footer__bottom-scroll">
-                                    <a href="#" data-target="html" class="scroll-to-target scroll-to-top"><i class="icon-up-arrow"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+<footer class="text-center text-lg-start bg-light text-muted" style="padding-bottom:0px;">
+    <section class="content bg1a1a1a">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="footer_link ">
+                        <li class="font_size"><a class="font_size" href="index.php">Home</a></li>
+                        <li class="font_size"><a class="font_size" href="about.php">About</a></li>
+                        <li class="font_size"><a class="font_size" href="service_agriculture.php">Services</a></li>
+                        <li class="font_size"><a class="font_size" href="blog_details_agriculture.php">Blogs</a></li>
+                        <li class="font_size"><a class="font_size" href="contactmin.php">Contact Us</a></li>
+
+                        <li class="font_size"><a class="font_size" href="cancelreturn.php">Cancel Return</a></li>
+                        <li class="font_size"><a class="font_size" href="shipping.php">Shipping</a></li>
+                        <li class="font_size"><a class="font_size" href="privacy_policy.php">Privacy Policy</a></li>
+                        <li class="font_size"><a class="font_size" href="terms.php">Terms & Conditions</a></li>
+                        <li class="font_size"><a class="font_size" href="gallery.php">Photo Gallery</a></li>
+                        <li class="font_size"><a class="font_size" href="news.php">E-News</a></li>
+                        <li class="font_size"><a class="font_size" href="contactmin.php">Contact</a></li>
+                    </ul>
                 </div>
             </div>
-        </footer>
-        <!--Site Footer End-->
+        </div>
+    </section>
+    <section class="content bg252525">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                        <h4 class="department_content font_size">
+                            Website Content Given & Managed by Shyamavsvss Krishi Limited 
+                            <!--| Last Updated: <span id="spnLastupdate">--><!--</span>-->
+                            <br />
+                            Designed and Developed by <a href="https://www.glintel.com/">Glintel Technologies</a>
+                            Visitors: <span id="span_visitor"></span> 
+                            
+                        </h4>
 
 
-    </div><!-- /.page-wrapper -->
-
-
-    <div class="mobile-nav__wrapper">
-        <div class="mobile-nav__overlay mobile-nav__toggler"></div>
-        <!-- /.mobile-nav__overlay -->
-        <div class="mobile-nav__content">
-            <span class="mobile-nav__close mobile-nav__toggler"><i class="fa fa-times"></i></span>
-
-            <div class="logo-box">
-                <a href="index.html" aria-label="logo image"><img src="assets/images/resources/logo-2.png" width="122"
-                        alt="" /></a>
+                    
+                </div>
             </div>
-            <!-- /.logo-box -->
-            <div class="mobile-nav__container"></div>
-            <!-- /.mobile-nav__container -->
-
-            <ul class="mobile-nav__contact list-unstyled">
-                <li>
-                    <i class="fa fa-envelope"></i>
-                    <a href="#">info@ssegr.org.in</a>
-                </li>
-                <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="#">91+ 9919412317</a>
-                </li>
-            </ul><!-- /.mobile-nav__contact -->
-            <div class="mobile-nav__top">
-                <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
-                    <a href="#" class="fab fa-instagram"></a>
-                </div><!-- /.mobile-nav__social -->
-            </div><!-- /.mobile-nav__top -->
-
-
-
         </div>
-        <!-- /.mobile-nav__content -->
-    </div>
-    <!-- /.mobile-nav__wrapper -->
 
-    <div class="search-popup">
-        <div class="search-popup__overlay search-toggler"></div>
-        <!-- /.search-popup__overlay -->
-        <div class="search-popup__content">
-            <form action="#">
-                <label for="search" class="sr-only">search here</label><!-- /.sr-only -->
-                <input type="text" id="search" placeholder="Search Here..." />
-                <button type="submit" aria-label="search submit" class="thm-btn">
-                    <i class="icon-magnifying-glass"></i>
-                </button>
-            </form>
-        </div>
-        <!-- /.search-popup__content -->
-    </div>
-    <!-- /.search-popup -->
+    </section>
 
-
-
-    <script src="assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+</footer>
+<script src="assets/vendors/jquery/jquery-3.6.0.min.js"></script>
     <script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="assets/vendors/jarallax/jarallax.min.js"></script>
     <script src="assets/vendors/jquery-ajaxchimp/jquery.ajaxchimp.min.js"></script>
@@ -235,8 +126,16 @@
 
     <!-- template js -->
     <script src="assets/js/agrion.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+ 
+window.setTimeout(function() {
+    $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
+        $(this).remove(); 
+    });
+}, 5000);
+ 
+});
+</script>
 </body>
-
-
-<!-- Mirrored from layerdrops.com/agrionhtml/main-html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 28 Oct 2022 06:18:43 GMT -->
 </html>
